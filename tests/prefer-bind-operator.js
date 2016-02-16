8d7fa60c09f194b211430a52d3d84564758225e1
Erik <erikdesjardins@users.noreply.github.com>
2016-02-15 20:10:19 -0500

    add tests for valid code

--- tests/prefer-bind-operator.js
+++ tests/prefer-bind-operator.js
@@ -12,6 +12,22 @@ var RuleTester = require('eslint').RuleTester;
 var ruleTester = new RuleTester();
 ruleTester.run('plugin-prefer-bind-operator', rule, {
 	valid: [
+		// variables
+		'var apply; var call; var bind;',
+		// properties
+		'a.apply; a.call; a.bind;',
+		// deep properties
+		'a.b.apply; a.b.call; a.b.bind;',
+		// too few arguments
+		'a.apply(); a.call(); a.bind();',
+		// deep properties, too few arguments
+		'a.b.apply(); a.b.call(); a.b.bind();',
+		// bare function calls
+		'apply(); call(); bind();',
+		// bare function calls with arguments
+		'apply(foo); call(foo); bind(foo);',
+		// bare function calls, multiple arguments
+		'apply(foo, bar); call(foo, bar); bind(foo, bar);'
 	],
 	invalid: [
 	]
